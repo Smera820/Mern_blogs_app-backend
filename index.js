7fe81f394d3bbe86bf3e22e4f12c5681bc95b019
smerasabu <smerasabu@gmail.com>
2025-07-05 12:31:50 +0530

    updated

--- index.js
+++ index.js
@@ -34,7 +34,7 @@ const ConnectDB = async () => {
 
 // middleware
 dotenv.config()
-app.use(express.json())
+app.use(express.json());
 
 app.use("/images", express.static(path.join(__dirname, "images")));
 console.log(cors())
